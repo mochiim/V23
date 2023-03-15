447f4bf7932dc9f1c9b5543b883756186ff18a77
mochiim <43903898+mochiim@users.noreply.github.com>
2023-03-15 09:19:21 +0100

    Density and EoS parameter finished. Fixing up in plotting left.

--- AST3220 Cosmology I/equationofmotion.py
+++ AST3220 Cosmology I/equationofmotion.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp
+from scipy.integrate import solve_ivp, cumulative_trapezoid
 
 plt.style.use("seaborn")
 plt.rcParams["font.size"] = 16 # font size of plots
@@ -8,10 +8,10 @@ plt.rcParams["font.size"] = 16 # font size of plots
 def power_law_potential(N, parameters):
     x1, x2, x3, lmbda = parameters
 
-    dx1 = -3 * x1 + np.sqrt(6) / 2 * lmbda * x2**2 + .5 * x1 * (3 + 3 * x1**2 - 3 * x2**2 + x3**2)
-    dx2 = -np.sqrt(6) / 2 * lmbda * x1 * x2 + .5 * x2 * (3 + 3 * x1**2 - 3 * x2**2 + x3**2)
-    dx3 = -2 * x3 + .5 * x3 * (3 + 3 * x1**2 - 3 * x2**2 + x3**2)
-    dlmbda = -np.sqrt(6) * lmbda**2 * x1
+    dx1 = - 3*x1 + (np.sqrt(6)/2)*lmbda*x2**2 + .5*x1*(3 + 3*x1**2 - 3*x2**2 + x3**2)
+    dx2 = - (np.sqrt(6)/2)*lmbda*x1*x2 + .5*x2*(3 + 3*x1**2 - 3*x2**2 + x3**2)
+    dx3 = - 2*x3 + .5*x3*(3 + 3*x1**2 - 3*x2**2 + x3**2)
+    dlmbda = - np.sqrt(6)*lmbda**2*x1
 
     differentials = np.array([dx1, dx2, dx3, dlmbda])
 
@@ -22,36 +22,17 @@ def exponential_potential(N, parameters):
     x1, x2, x3 = parameters
 
     lmbda = 3/2
-    dx1 = -3 * x1 + np.sqrt(6) / 2 * lmbda * x2**2 + .5 * x1 * (3 + 3 * x1**2 - 3 * x2**2 + x3**2)
-    dx2 = -np.sqrt(6) / 2 * lmbda * x1 * x2 + .5 * x2 * (3 + 3 * x1**2 - 3 * x2**2 + x3**2)
-    dx3 = -2 * x3 + .5 * x3 * (3 + 3 * x1**2 - 3 * x2**2 + x3**2)
+    dx1 = - 3*x1 + (np.sqrt(6)/2)*lmbda*x2**2 + .5*x1*(3 + 3*x1**2 - 3*x2**2 + x3**2)
+    dx2 = - (np.sqrt(6)/2)*lmbda*x1*x2 + .5*x2*(3 + 3*x1**2 - 3*x2**2 + x3**2)
+    dx3 = - 2*x3 + .5*x3*(3 + 3*x1**2 - 3*x2**2 + x3**2)
 
     differentials = np.array([dx1, dx2, dx3])
 
     return differentials
 
-def integration(type):
+def integration(function, init):
 
-    if type == "power":
-        # initial conditions
-        x1_init = 5e-5
-        x2_init = 1e-8
-        x3_init = .9999
-        lmbda_init = 1e9
-
-        init = np.array([x1_init, x2_init, x3_init, lmbda_init])
-
-        solve = solve_ivp(power_law_potential, z_span, init, method = 'RK45', rtol = 1e-8, atol = 1e-8, dense_output = True)
-
-
-    if type == "exp":
-        # initial conditions
-        x1_init = 0
-        x2_init = 5e-13
-        x3_init = .9999
-
-        init = np.array([x1_init, x2_init, x3_init])
-        solve = solve_ivp(exponential_potential, z_span, init, method = 'RK45', rtol = 1e-8, atol = 1e-8, dense_output = True)
+    solve = solve_ivp(function, z_span, init, method = 'RK45', rtol = 1e-8, atol = 1e-8, dense_output = True)
 
     exes = solve.sol(N)
 
@@ -75,15 +56,35 @@ z_span = [-np.log(1 + 2e7), 0]
 steps = 1000
 N = np.linspace(-np.log(1 + 2e7), 0, steps)
 
-
 z = np.exp(- N) - 1
 
-omega_m, omega_phi, omega_r, w_phi = integration("power")
+variables_power = integration(power_law_potential, np.array([5e-5, 1e-8, .9999, 1e9]))
+variables_exp = integration(exponential_potential, np.array([0, 5e-13, .9999]))
 
-plt.plot(z, omega_m)
-plt.plot(z, omega_phi)
-plt.plot(z, omega_r)
-plt.plot(z, w_phi)
+labels_power = [r"$\Omega_m$", r"$\Omega_\phi$", r"$\Omega_r$", r"$w_\phi$"]
+
+"""
+for i in range(4):
+    plt.plot(z, variables_power[i])
+
 plt.xscale("log")
-plt.tight_layout()
+plt.legend(labels_power)
+plt.show()
+"""
+
+def Hubble_parameter(omega_m0, omega_phi0, omega_r0, EoS):
+    w_phi = EoS
+    integ = cumulative_trapezoid(3 * (1 + w_phi), N, initial = 0)
+    H = np.sqrt(omega_m0 * np.exp(-3 * N) + omega_r0 * np.exp(-4 * N) + omega_phi0 * np.exp(integ))
+    return H
+
+H_exp = Hubble_parameter(variables_exp[0], variables_exp[1], variables_exp[2], variables_exp[3])
+H_power = Hubble_parameter(variables_power[0], variables_power[1], variables_power[2], variables_power[3])
+omega_m0CDM = .3
+H_CDM = np.sqrt(omega_m0CDM * np.exp(-3 * N) + (1 - omega_m0CDM))
+plt.plot(z, H_exp)
+plt.plot(z, H_power)
+plt.plot(z, H_CDM)
+plt.xscale("log")
+plt.yscale("log")
 plt.show()
