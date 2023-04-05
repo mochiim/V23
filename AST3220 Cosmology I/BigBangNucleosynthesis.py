b7033eaa114b16c86192d596df91869b396e2626
mochiim <43903898+mochiim@users.noreply.github.com>
2023-04-05 20:40:40 +0200

    ...

---
+++ AST3220 Cosmology I/BigBangNucleosynthesis.py
@@ -0,0 +1,141 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.constants as const
+from scipy.integrate import solve_ivp, quad
+
+plt.style.use("seaborn")
+plt.rcParams['font.size'] = 20
+plt.rcParams["lines.linewidth"] = 2
+
+class BigBangNucleosynthesis():
+    def __init__(self):
+        # defining useful constants in CGS units
+        self.c = const.c.cgs.value               # speed of light in vacuum
+        self.k = const.k_B.cgs.value             # Boltzmann constant
+        self.hbar = const.hbar.cgs.value         # reducced Planck constant
+        self.G = const.G.cgs.value               # Newton's gravitational constant
+        self.m_e = const.m_e.cgs.value           # electron mass
+        self.m_p = const.m_p.cgs.value           # proton mass
+        self.m_n = const.m_n.cgs.value           # neutron mass
+
+        self.h = 0.7                             # Hubble constant [-]
+        self.H0 = 100*self.h*1e5/3.09e+24        # Hubble parameter in CGS units
+        self.T0 = 2.72548                        # temperature today of CMB [K]
+        self.N_eff = 3                           # neutrino species [-]
+        self.Omega_b0 = 0.05
+        self.rho_c0 = 1.879e-29*self.h**2        # critial density [gcm^-3]
+
+    def differentials_f(self, logT, Y):
+        """
+        This function includes differential equations for dY_i/d(lnT) for i = n, p.
+        The differential equations are given in eq. (10) and (11) in the task description.
+        """
+        T = np.exp(logT)
+        a = self.T0/T       # scale factor
+
+        Omega_r0 = (8*np.pi**3*self.G*(self.k*self.T0)**4)/(45*self.H0**2*self.hbar**3*self.c**5)*(1 + (7/8)*self.N_eff*(4/11)**(4/3))
+        H = self.H0*np.sqrt(Omega_r0)*a**(-2) # eq. 14 in task description
+        Y_n = Y[0]; Y_p = Y[1]        # unpack values from Y and assign names
+
+        dY_n = -1 / H*(Y_p*self.gamma(T, "proton to neutron") - Y_n*self.gamma(T, "neutron to proton") )
+        dY_p = -1 / H*(Y_n*self.gamma(T, "neutron to proton") - Y_p*self.gamma(T, "proton to neutron") )
+
+        return np.array([dY_n, dY_p])
+
+    def taskf(self):
+        """
+        Solving differential equations for dY_i/d(lnT) for i = n, p and plotting results.
+        """
+        # defining temperature interval
+        T_i = 100e9; T_f = .1e9 # [K]
+        Y_n_Ti = (1 + np.exp((self.m_n - self.m_p)*self.c**2/self.k*T_i ))**(-1) # from task e)
+        Y_p_Ti = 1 - Y_n_Ti # from task e)
+
+        solve = solve_ivp(self.differentials_f, [np.log(T_i), np.log(T_f)], [Y_n_Ti, Y_p_Ti], method= "Radau", rtol = 1e-12, atol = 1e-12)
+
+        T = np.exp(solve.t)
+        Y_n = solve.y[0]
+        Y_p = solve.y[1]
+
+        plt.plot(T, Y_p, label = "p", color = "blue")
+        plt.plot(T, Y_n, label = "n", color = "orange")
+        plt.plot(T, 1/(1 + np.exp((self.m_n-self.m_p)*self.c**2/(self.k*T))), linestyle = "dotted", color = "tab:orange")
+        plt.plot(T, 1 - 1/(1 + np.exp((self.m_n-self.m_p)*self.c**2/(self.k*T))), linestyle = "dotted", color = "tab:blue")
+        plt.xscale("log"); plt.yscale("log")
+
+        plt.xlim([T[0], T[-1]]); plt.ylim([1e-3, 2]) # matching the plotting window of figure we are attempting to reproduce
+        plt.xlabel('$T$ [K]'); plt.ylabel(r'$Y_i$')
+        plt.legend()
+        #plt.savefig("f.png")
+
+    def differentials_h(self, logT, Y):
+        T = np.exp(logT)
+        a = self.T0/T       # scale factor
+        T_9 = T/1e9
+
+        Omega_r0 = (8*np.pi**3*self.G*(self.k*self.T0)**4)/(45*self.H0**2*self.hbar**3*self.c**5)*(1 + (7/8)*self.N_eff*(4/11)**(4/3))
+        H = self.H0*np.sqrt(Omega_r0)*a**(-2) # eq. 14 in task description
+        Y_n = Y[0]; Y_p = Y[1]; Y_D = Y[2]        # unpack values from Y and assign names
+
+        rho_b = self.Omega_b0*self.rho_c0/a**3
+        pn = 2.5e4*rho_b
+        lmbda = 4.68e9*pn/rho_b*T_9**(3/2)*np.exp(- 25.82/T_9)
+
+        dY_n = -1/H*(Y_p*self.gamma(T, "proton to neutron") - Y_n*self.gamma(T, "neutron to proton"))
+        dY_p = -1/H*(Y_n*self.gamma(T, "neutron to proton") - Y_p*self.gamma(T, "proton to neutron"))
+        dY_D = -1/H*(- lmbda*Y_D + pn*Y_n*Y_p)
+
+        return np.array([dY_n, dY_p, dY_D])
+
+    def taskh(self):
+        # defining temperature interval
+        T_i = 100e9; T_f = .1e9 # [K]
+        Y_n_Ti = (1 + np.exp((self.m_n - self.m_p)*self.c**2/self.k*T_i ))**(-1) # from task e)
+        Y_p_Ti = 1 - Y_n_Ti # from task e)
+        Y_D_Ti = 0
+
+        solve = solve_ivp(self.differentials_h, [np.log(T_i), np.log(T_f)], [Y_n_Ti, Y_p_Ti, Y_D_Ti], method = "Radau", rtol = 1e-12, atol = 1e-12)
+
+        T = np.exp(solve.t)
+        Y_n = solve.y[0]
+        Y_p = solve.y[1]
+        Y_D = solve.y[2]
+
+        plt.plot(T, Y_p, label = "p", color = "blue")
+        plt.plot(T, Y_n, label = "n", color = "orange")
+        plt.plot(T, 2*Y_D, label = 'D')
+        plt.plot(T, 1/(1 + np.exp((self.m_n-self.m_p)*self.c**2/(self.k*T))), linestyle = "dotted", color = "tab:orange")
+        plt.plot(T, 1 - 1/(1 + np.exp((self.m_n-self.m_p)*self.c**2/(self.k*T))), linestyle = "dotted", color = "tab:blue")
+        plt.xscale("log"); plt.yscale("log")
+
+        plt.xlim([T[0], T[-1]]); plt.ylim([1e-3, 2]) # matching the plotting window of figure we are attempting to reproduce
+        plt.xlabel('$T$ [K]'); plt.ylabel(r'$Y_i$')
+        plt.legend()
+        #plt.savefig("h.png")
+
+    def gamma(self, T, type):
+        """
+        Decay rates (eq. 12 in task description) from table 2
+        """
+        T_nu = (4/11)**(1/3)*T # from task b)
+        T_9 = T/1e9
+        T_9nu = T_nu/1e9
+        Z = 5.93/T_9
+        Z_nu = 5.93/T_9nu
+        tau = 1700 # free neutron decay time [s]
+
+        if type == "neutron to proton":
+            q = 2.53
+        elif type == "proton to neutron":
+            q = - 2.53
+
+        f = lambda x: ((x+q)**2*(x**2-1)**(1/2)*x)/((1 + np.exp(x*Z))*(1 + np.exp(-(x+q)*Z_nu ))) \
+            + ((x-q)**2*(x**2-1)**(1/2)*x)/((1 + np.exp(-x*Z))*(1 + np.exp((x - q)*Z_nu)))
+
+        x = np.linspace(1, 100, 1000)
+        return 1/tau*np.trapz(f(x), dx = (x[1] - x[0]))
+
+
+#BigBangNucleosynthesis().taskf()
+BigBangNucleosynthesis().taskh()
+plt.show()
--- AST3220 Cosmology I/test.py
+++ AST3220 Cosmology I/test.py
@@ -1,147 +1,89 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp, cumulative_trapezoid, simpson, quad
-from scipy.interpolate import CubicSpline
-from tabulate import tabulate
+import scipy.integrate as int
+import astropy.constants as const
+from scipy import interpolate
+
+
+c = const.c.cgs.value         # light speed
+k_B = const.k_B.cgs.value     # Boltzmann constant
+G = const.G.cgs.value         # Newton's gravitational constant
+h_bar = const.hbar.cgs.value  # reduced Planck constant
+m_e = const.m_e.cgs.value     # electron mass
+m_p = const.m_p.cgs.value     # proton mass
+m_n = const.m_n.cgs.value     # neutron mass
+
+h = 0.7                # hubble constant
+N_eff = 3              # neutrino species
+T0 = 2.725             # [K] today's temperature of the CMB
+H0 = 100 * h * 1e5 / 3.086e24  # Hubble parameter in CGS
+tau = 1700             # [s] free neutron decay time
+
+
+def dY_f(logT, Y):
+        """
+        Differential eqs for dYn/dlnT, dYp/dlnT from eqs (10), (11).
+        Work out dY/dlnT for the first two weak reactions in table 2.
+        """
+        T = np.exp(logT) # [K] temperature
+        a = 0 / T
+
+        omega_r0 = (8 * np.pi**3 * G) / (90 * H0**2) \
+                    * (k_B * T0)**4 / (h_bar**3 * c**5) \
+                    * (2 + N_eff * 7/4 * (4/11)**(4/3))
+
+        H = H0 * np.sqrt(omega_r0) * a**(-2)
+        Y_n = Y[0]; Y_p = Y[1]
+
+
+        dY_n = -1 / H * (Y_p * gamma_p_n(T) - Y_n * gamma_n_p(T))
+        dY_p = -1 / H * (Y_n * gamma_n_p(T)- Y_p * gamma_p_n(T))
+
+        return np.array([dY_n, dY_p])
+
+def task_f():
+        """Solve differential equations for dYn and dYp and plot the results.
+        """
+        # inial conditions for Y_n  and Y_p:
+        T_i = 1e11
+        Yn_Ti = (1 + np.exp( (m_n - m_p) \
+                            * c**2 / k_B * T_i ) )**(-1)
+        Yp_Ti = 1 - Yn_Ti
+
+        solve = int.solve_ivp(dY_f, t_span=[np.log(100e9), np.log(0.1e9)],\
+                y0=[Yn_Ti, Yp_Ti], method='Radau', rtol=1e-12, atol=1e-12)
+
+        T = np.exp(solve.t)
+        Y_n = solve.y[0]
+        Y_p = solve.y[1]
+
+        plt.plot(T, Y_p, label='p')
+        plt.plot(T, Y_n, label='n')
+
+        plt.show()
 
 
 
-""" Density parameters and equation of state """
-
-def power_law_potential(N, parameters):
-    """
-    KOMMENTER
-    """
-    x1, x2, x3, lmbda = parameters
-
-    dx1 = - 3*x1 + (np.sqrt(6)/2)*lmbda*x2**2 + .5*x1*(3 + 3*x1**2 - 3*x2**2 + x3**2)
-    dx2 = - (np.sqrt(6)/2)*lmbda*x1*x2 + .5*x2*(3 + 3*x1**2 - 3*x2**2 + x3**2)
-    dx3 = - 2*x3 + .5*x3*(3 + 3*x1**2 - 3*x2**2 + x3**2)
-    dlmbda = - np.sqrt(6)*lmbda**2*x1
-
-    return dx1, dx2, dx3, dlmbda
 
 
-def exponential_potential(N, parameters):
-    x1, x2, x3 = parameters
 
-    lmbda = 3/2
-    dx1 = - 3*x1 + (np.sqrt(6)/2)*lmbda*x2**2 + .5*x1*(3 + 3*x1**2 - 3*x2**2 + x3**2)
-    dx2 = - (np.sqrt(6)/2)*lmbda*x1*x2 + .5*x2*(3 + 3*x1**2 - 3*x2**2 + x3**2)
-    dx3 = - 2*x3 + .5*x3*(3 + 3*x1**2 - 3*x2**2 + x3**2)
+def gamma_n_p(T, q = 2.53):
 
-    return dx1, dx2, dx3
+        T_nu = (4/11)**(1/3) * T # From task b)
+        Z_nu = m_e * c**2 / (k_B * T_nu)
+        Z = m_e * c**2 / (k_B * T)
 
-def integration(function, init):
-    """
-    A function which solves a differential equation using solve_ivp for a
-    function [argument: function] and its initial conditions at z = 2e7
-    [argument: init]. This results in x1, x2 and x3 which can be used to find
-    density parameters and EoS parameter.
-    """
-    # ODE solver
-    solve = solve_ivp(function, N_span, init, method = 'RK45', rtol = 1e-8, atol = 1e-8, dense_output = True)
+        f = lambda x: ((x + q)**2 * (x**2 - 1)**(1/2) * x) \
+            / ((1 + np.exp(x * Z)) * (1 + np.exp( -(x + q) * Z_nu ))) \
+            + ((x - q)**2 * (x**2 - 1)**(1/2) * x ) \
+            / ((1 + np.exp(-x * Z)) * (1 + np.exp( (x - q) * Z_nu )))
 
-    # solve for x1, x2 and x3
-    exes = solve.sol(N)
+        x = np.linspace(1, 100, 1000)
+        dx = x[1] - x[0]
+        return 1/tau * np.trapz(f(x), dx=dx)
 
-    # unpack exes and asign names
-    x1 = exes[0, :]
-    x2 = exes[1, :]
-    x3 = exes[2, :]
-
-    # densitiy parameters
-    omega_m = 1 - x1**2 - x2**2 - x3**2
-    omega_phi = x1**2 + x2**2
-    omega_r = x3**2
-
-    # EoS parameter
-    w_phi = (x1**2 - x2**2) / (x1**2 + x2**2)
-
-    return omega_m, omega_phi, omega_r, w_phi
-
-# we define a new variable N for easier calculation
-N_span = [-np.log(1 + 2e7), 0]
-steps = 1000
-N = np.linspace(-np.log(1 + 2e7), 0, steps)
-
-# convert N back to z
-z = np.exp(- N) - 1
-
-# computation of density parameter and EoS for two Quintessence models
-variables_power = integration(power_law_potential, np.array([5e-5, 1e-8, .9999, 1e9]))
-variables_exp = integration(exponential_potential, np.array([0, 5e-13, .9999]))
-
-""" Hubble parameter """
-
-def Hubble_parameter(omega_m0, omega_phi0, omega_r0, EoS):
-    w_phi = EoS
-    integration = cumulative_trapezoid(3*(1 + np.flip(w_phi)), N, initial = 0) # initial 0 to avoid loss of one element
-    H = np.sqrt(omega_m0 * np.exp(-3 * N) + omega_r0 * np.exp(-4 * N) + omega_phi0 * np.exp(np.flip(integration)))
-    return H
-
-H_exp = Hubble_parameter(variables_exp[0][-1], variables_exp[1][-1], variables_exp[2][-1], variables_exp[3])
-H_power = Hubble_parameter(variables_power[0][-1], variables_power[1][-1], variables_power[2][-1], variables_power[3])
-omega_m0CDM = .3
-H_CDM = np.sqrt(omega_m0CDM * np.exp(-3 * N) + (1 - omega_m0CDM))
-
-""" Age of the universe """
-def age_of_universe(Hubble_parameter):
-    t_0 = simpson(1/Hubble_parameter, N)
-    return t_0
+def gamma_p_n(T, q=2.53):
+        return gamma_n_p(T, -q)
 
 
-""" Luminosity distance """
-
-def luminosity_distance(Hubble_parameter):
-    """
-    Computing the luminosity distance for a given Hubble parameter
-    """
-    search = np.logical_and(N <= - np.log(3) + 1e-2, N  >= - np.log(3) - 1e-2)
-    idx = np.where(search == True)[0][0]
-    N_reduced = N[idx:]
-    H = Hubble_parameter[idx:]
-    integration = cumulative_trapezoid(np.exp(- N_reduced)/H, N_reduced, initial = 0)
-    dL = np.exp(- N_reduced)*np.flip(integration)
-
-    z_dL = z[idx:]
-
-    return z_dL, dL
-
-""" problem 12 """
-
-z_dL, dL_power = luminosity_distance(H_power)  # unitless
-z_dL, dL_exp = luminosity_distance(H_exp)      # unitless
-
-""" problem 13 """
-z_data, dL_data, error_data = np.loadtxt('/Users/rebeccanguyen/Documents/GitHub/V23/AST3220 Cosmology I/sndata.txt', skiprows=5, unpack=True)
-
-h = 0.7
-
-# luminosity distance converted to units of length
-dL_power_adjusted = dL_power*(3/h)   # [Gpc]
-dL_exp_adjusted = dL_exp*(3/h)       # [Gpc]
-
-
-def chisquared(model):
-    z_dL, dL = model
-    chi = 0
-    for i, z in enumerate(z_data):
-        interpol = CubicSpline(np.flip(z_dL), np.flip(dL))
-        value = np.interp(z, np.flip(z_dL), interpol(np.flip(z_dL)))
-        chi += (value - dL_data[i])**2 / error_data[i]**2
-    return chi
-
-#chisquared()
-print(chisquared([z_dL, dL_power_adjusted]))
-print(chisquared([z_dL, dL_exp_adjusted]))
-
-"""
-interpol = CubicSpline(np.flip(z_dL), np.flip(dL_exp_adjusted))
-value = np.interp(0.799, np.flip(z_dL), interpol(np.flip(z_dL)))
-plt.scatter(0.799, value)
-plt.plot(z_dL, np.flip(interpol)(z_dL))
-plt.plot(z_data, dL_data)
-plt.show()
-"""
+task_f()
