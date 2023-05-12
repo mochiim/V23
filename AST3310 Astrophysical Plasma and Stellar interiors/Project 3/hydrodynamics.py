3683fe2cca2b1ee0e625d46a1d6ff9e6f164ce8c
mochiim <43903898+mochiim@users.noreply.github.com>
2023-05-12 20:38:24 +0200

    u not initialised

--- AST3310 Astrophysical Plasma and Stellar interiors/Project 3/hydrodynamics.py
+++ AST3310 Astrophysical Plasma and Stellar interiors/Project 3/hydrodynamics.py
@@ -12,27 +12,43 @@ class 2Dconvection:
         # constants
         self.k = sc.k           # Boltzmann constant [J K^-1]
         self.G = sc.G           # Newtonian constant of gravitation [m^3 kg^-1 s^-2]
+        self.m_u = sc.m_u       # atomic mass unit [kg]
 
         # model
         self.yaxis = 4e6        # length of box (vertical) [m]
         self.xaxis = 12e6       # width of box (horizontal) [m]
         self.Ny = 100           # number of boxes in vertical direction
         self.Nx = 300           # number of boxes in horizontal direction
+        self.dx = self.xaxis/self.Nx   
+        self.dy = self.yaxis/self.Ny
         self.mu = .61           # mean mulecular weight
         
         # solar attributes
         self.T_sun = 5778                            # temperature of photosphere [K]
         self.P_sun = 1.8e4                           # pressure of photosphere [Pa]
+        self.rho_sun = 2.3e-4                        # density of photosphere [kg m^-3]
         self.R_sun = 6.96e8                          # solar radius [m]
         self.M_sun = 1.989e30                        # solar mass [kg]
         self.g = - self.G*self.M_sun/self.R_sun**2   # constant gravitational acceleration in negative y-direction
 
+        # arrays to store calculated values
+        self.T = np.zeros([self.Nx, self.Ny])
+        self.P = np.zeros([self.Nx, self.Ny])
+        self.rho = np.zeros([self.Nx, self.Ny])
+        self.u = np.zeros([self.Nx, self.Ny])
+        self.e = np.zeros([self.Nx, self.Ny])
 
     def initialise(self):
 
         """
         initialise temperature, pressure, density and internal energy
         """
+        nabla = 2.5 + .01
+
+        P = self.G * self.M_sun * self.rho_sun / self.R_sun ** 2
+        T = self.T_sun - nabla * self.T_sun / self.P_sun * P
+        e = 3/2 * self.P
+        rho = P * self.mu * self.m_u / ( self.k * T)
 
     def timestep(self):
 
