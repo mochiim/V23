98147659c3791357f5d57f058e1035fd13855b24
mochiim <43903898+mochiim@users.noreply.github.com>
2023-06-10 21:10:40 +0200

    Finished task f

--- AST3220 Cosmology I/project 3.py
+++ AST3220 Cosmology I/project 3.py
@@ -10,30 +10,90 @@ c = ast.c.value                       # speed of light in vacuum [m/s]
 E_P = np.sqrt((hbar * c ** 5) / G)    # Planck energy
 
 e_folds = 500
-phi_init = E_P * np.sqrt((e_folds + 1/2 ) / (2*np.pi))    # initial value for scalar field 
-print(f"Initial value for the field that will give 500 e-folds of inflation is ", "{:e}".format(phi_init))
+phi_init = E_P * np.sqrt((e_folds + 1/2 ) / (2*np.pi))    # initial value for the field                                 
 
 """ Task e"""
-v = lambda 
-psi = lambda psi: phi / E_P
-dv_dpsi = lambda psi: 3 * hbar * c^2 * psi / (4 * np.pi * G * phi_i **2)
-dpsi_dtau = lambda h, dpsi, dv: - 3 * h * dpsi - dv
+
+_psi = lambda phi: phi/E_P
+_dv_dpsi = lambda psi: 3 * hbar * c ** 5 / (4 * np.pi * G) * psi / phi_init ** 2
+_v = lambda psi: (3 * hbar * c ** 5) / (8 *np.pi * G) * (psi / phi_init) ** 2
+
+def _solver(n = int(1e5)):
+
+    h = np.zeros(n)
+    psi = np.zeros(n)
+    dpsi = np.zeros(n)
+    ln_a_ai = np.zeros(n)
+
+    # defining steplength and time interval
+    tau_end = int(1e4) # arbitrary value
+    dtau = tau_end / n
+    tau = np.linspace(0, tau_end, n)
+
+    # initial conditions
+    h[0] = 1
+    psi[0] = _psi(phi_init)
+    dpsi[0] = 0
+    ln_a_ai[0] = 0
+
+    for i in range(n - 1):
+        d2psi = - 3 * h[i] * dpsi[i] - _dv_dpsi(psi[i]) # double derivative of scalar field
+
+        dpsi[i+1] = dpsi[i] + d2psi * dtau
+
+        psi[i+1] = psi[i] + dpsi[i] * dtau
+
+        h[i+1] = np.sqrt( np.abs( 8 * np.pi / 3 * (0.5 * dpsi[i + 1] ** 2 + _v(psi[i + 1])) ))
+
+        ln_a_ai[i + 1] = ln_a_ai[i] + h[i] * dtau
+
+    return h, tau, psi, dpsi, ln_a_ai
+
+def _taske(save = False):
+    h, tau, psi, dpsi, ln_a_ai = _solver()
+    fig, ax = plt.subplots(2, sharex=True, figsize = (6, 6))
+    ax[0].plot(tau, psi/_psi(phi_init), color = "black")
+    ax[0].set_ylabel(r"$\psi / \psi_i$")
+    ax[0].set_xlim([0, 2000])
+
+    ax[1].plot(tau, ln_a_ai, color = "black")
+    ax[1].set_ylabel(r"ln(a/a$_i$)")
+    ax[1].set_xlabel(r"$\tau$")
+    ax[1].set_xlim([0, 2000])
+
+    # create a zoom in window
+    axins = ax[0].inset_axes([.75, .2, .2, .7])
+    axins.plot(tau, psi/_psi(phi_init), color = "black")
+    x1, x2, y1, y2 = 1010, 1410, -0.02, .02
+    axins.set_xlim(x1, x2)
+    axins.set_ylim(y1, y2)
+    axins.tick_params(labelbottom = False, labelleft = False)
+    ax[0].indicate_inset_zoom(axins, edgecolor = "black")
+
+    if save:
+        plt.savefig("task_e.png")
+
+""" Task f"""
+
+def _taskf(save = False):
+    """
+    SRA
+    """
+    h, tau, psi, dpsi, ln_a_ai = _solver()
+    SRA_psi = _psi(phi_init) - tau / (4 * np.pi *_psi(phi_init))
+    plt.figure(figsize=(6, 6))
+    plt.plot(tau, psi, color = "black", label = "Numerical solution")
+    plt.plot(tau, SRA_psi, label = "Slow-roll approximation", color = "black", ls = "dashed")
+    plt.xlim([0, 2000]); plt.ylim([-3, 4])
+    plt.xlabel(r"$\tau$"), plt.ylabel(r"$\psi$")
+    plt.legend()
+
+    if save:
+        plt.savefig("task_f.png")
+
+if __name__ == "__main__":
+    #_taske(save = False)
+    #_taskf(save = False)
+    plt.show()
+ 
     
-
-def solve(N = 10):
-    dtau = 1e-3
-    tau = np.linspace(0, N, 1000)
-
-    # array for storage
-    h = np.zeros(N)
-    psi = np.zeros(N)
-    dpsi = np.zeros(N)
-    ln_a_ai = np.zeros(N)
-    
-    # initial values
-
-    for i in range(N):
-        ln_a_ai[i + 1] = ln_a_ai + h[i] * dtau
-        dpsi[i + 1] = dpsi[i] + psi_double_dot[i] * dtau
-        psi[i + 1] = psi[i] + dpsi[i] * dtau
-        h[i + 1] = h[i]
