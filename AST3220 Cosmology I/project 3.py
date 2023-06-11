44d8cec4fb8abb12fff8a9c8b33d1839096047d6
mochiim <43903898+mochiim@users.noreply.github.com>
2023-06-11 15:00:06 +0200

    Task g solved

--- AST3220 Cosmology I/project 3.py
+++ AST3220 Cosmology I/project 3.py
@@ -16,10 +16,10 @@ phi_init = E_P * np.sqrt((e_folds + 1/2 ) / (2*np.pi))    # initial value for th
 
 _psi = lambda phi: phi/E_P
 _dv_dpsi = lambda psi: 3 * hbar * c ** 5 / (4 * np.pi * G) * psi / phi_init ** 2
-_v = lambda psi: (3 * hbar * c ** 5) / (8 *np.pi * G) * (psi / phi_init) ** 2
+_v = lambda psi: 3/(8*np.pi) * (psi / _psi(phi_init))**2
 
 def _solver(n = int(1e5)):
-
+    # empty arrays for storing calculated values
     h = np.zeros(n)
     psi = np.zeros(n)
     dpsi = np.zeros(n)
@@ -49,51 +49,80 @@ def _solver(n = int(1e5)):
 
     return h, tau, psi, dpsi, ln_a_ai
 
-def _taske(save = False):
+def _taske(plot = False, save = False):
     h, tau, psi, dpsi, ln_a_ai = _solver()
-    fig, ax = plt.subplots(2, sharex=True, figsize = (6, 6))
-    ax[0].plot(tau, psi/_psi(phi_init), color = "black")
-    ax[0].set_ylabel(r"$\psi / \psi_i$")
-    ax[0].set_xlim([0, 2000])
 
-    ax[1].plot(tau, ln_a_ai, color = "black")
-    ax[1].set_ylabel(r"ln(a/a$_i$)")
-    ax[1].set_xlabel(r"$\tau$")
-    ax[1].set_xlim([0, 2000])
+    if plot:
+        fig, ax = plt.subplots(2, sharex=True, figsize = (6, 6))
+        ax[0].plot(tau, psi/_psi(phi_init), color = "black")
+        ax[0].set_ylabel(r"$\psi / \psi_i$")
+        ax[0].set_xlim([0, 2000])
 
-    # create a zoom in window
-    axins = ax[0].inset_axes([.75, .2, .2, .7])
-    axins.plot(tau, psi/_psi(phi_init), color = "black")
-    x1, x2, y1, y2 = 1010, 1410, -0.02, .02
-    axins.set_xlim(x1, x2)
-    axins.set_ylim(y1, y2)
-    axins.tick_params(labelbottom = False, labelleft = False)
-    ax[0].indicate_inset_zoom(axins, edgecolor = "black")
+        ax[1].plot(tau, ln_a_ai, color = "black")
+        ax[1].set_ylabel(r"ln(a/a$_i$)")
+        ax[1].set_xlabel(r"$\tau$")
+        ax[1].set_xlim([0, 2000])
+
+        # create a zoom in window
+        axins = ax[0].inset_axes([.75, .2, .2, .7])
+        axins.plot(tau, psi/_psi(phi_init), color = "black")
+        x1, x2, y1, y2 = 1010, 1410, -0.02, .02
+        axins.set_xlim(x1, x2)
+        axins.set_ylim(y1, y2)
+        axins.tick_params(labelbottom = False, labelleft = False)
+        ax[0].indicate_inset_zoom(axins, edgecolor = "black")
 
     if save:
         plt.savefig("task_e.png")
 
 """ Task f"""
 
-def _taskf(save = False):
+def _taskf(plot = False, save = False):
     """
     SRA
     """
     h, tau, psi, dpsi, ln_a_ai = _solver()
     SRA_psi = _psi(phi_init) - tau / (4 * np.pi *_psi(phi_init))
-    plt.figure(figsize=(6, 6))
-    plt.plot(tau, psi, color = "black", label = "Numerical solution")
-    plt.plot(tau, SRA_psi, label = "Slow-roll approximation", color = "black", ls = "dashed")
-    plt.xlim([0, 2000]); plt.ylim([-3, 4])
-    plt.xlabel(r"$\tau$"), plt.ylabel(r"$\psi$")
-    plt.legend()
+    
+    if plot:
+        plt.figure(figsize=(6, 6))
+        plt.plot(tau, psi, color = "black", label = "Numerical solution")
+        plt.plot(tau, SRA_psi, label = "Slow-roll approximation", color = "black", ls = "dashed")
+        plt.xlim([0, 2000]); plt.ylim([-3, 4])
+        plt.xlabel(r"$\tau$"), plt.ylabel(r"$\psi$")
+        plt.legend()
 
     if save:
         plt.savefig("task_f.png")
 
+""" Task g"""
+_epsilon = lambda phi: E_P ** 2 / (4 * np.pi * phi ** 2)
+
+def _taskg(plot = False, save = False):
+    h, tau, psi, dpsi, ln_a_ai = _solver()
+    phi = psi * E_P
+    epsilon = _epsilon(phi)
+
+    end_of_inflation = np.where(epsilon >= 1)[0][0] # finding index for where inflation ends, i.e. epsilon = 1
+    N_tot = ln_a_ai[end_of_inflation] # number of e-folds
+    print(f"Numerical N_tot: {N_tot}")
+
+    if plot:
+        plt.figure(figsize = (6, 6))
+        plt.plot(tau, epsilon, color = "black")
+        plt.xlabel(r"$\tau$")
+        plt.ylabel(r"$\epsilon$")
+        plt.yscale("log")
+        plt.xlim([0, 1500])
+
+    if save:
+        plt.savefig("task_g.png")
+
+
 if __name__ == "__main__":
-    #_taske(save = False)
-    #_taskf(save = False)
+    _taske(plot = False, save = False)
+    _taskf(plot = False, save = False)
+    _taskg(plot = False, save = False)
     plt.show()
  
     
